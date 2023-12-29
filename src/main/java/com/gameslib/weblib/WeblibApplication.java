c580d5cf3472ae8f5a89c490d8caf3222b12936e
epdlahtm92 <epdlahtm92@gmail.com>
2023-12-29 15:55:40 +0900

    Start Project 1229

---
+++ src/main/java/com/gameslib/weblib/WeblibApplication.java
@@ -0,0 +1,13 @@
+package com.gameslib.weblib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblibApplication.class, args);
+	}
+
+}
